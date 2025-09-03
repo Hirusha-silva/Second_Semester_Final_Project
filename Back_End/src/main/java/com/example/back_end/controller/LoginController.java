170cbaab442a620f2b7b5b0ce0bd0cb3cfc4d81a
Hirusha-silva <hirushasilva222@gmail.com>
2025-09-04 01:16:10 +0530

    login controller

--- Back_End/src/main/java/com/example/back_end/controller/LoginController.java
+++ Back_End/src/main/java/com/example/back_end/controller/LoginController.java
@@ -1,10 +1,13 @@
 package com.example.back_end.controller;
 
+import com.example.back_end.dto.ApiResponseDto;
+import com.example.back_end.dto.LoginDto;
+import com.example.back_end.dto.RegisterDto;
 import com.example.back_end.service.UserService;
+import com.example.back_end.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -12,5 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class LoginController {
     private final UserService userService;
+    private final UserServiceImpl userServiceImpl;
 
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponseDto> register(@RequestBody RegisterDto registerDto) {
+        return ResponseEntity.ok(
+                new ApiResponseDto(
+                        200,
+                        "User Registered Successfully",
+                        userService.registerUser(registerDto)
+                )
+        );
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponseDto> login(@RequestBody LoginDto loginDto) {
+        return ResponseEntity.ok(
+                new ApiResponseDto(
+                        200,
+                        "ok",
+                        userServiceImpl.authenticate(loginDto)
+                )
+        );
+    }
 }
