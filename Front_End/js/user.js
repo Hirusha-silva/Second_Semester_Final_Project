4dfc85289679b5b55d724b44344a46aee0864a15
Hirusha-silva <hirushasilva222@gmail.com>
2025-09-21 10:46:25 +0530

    update style myads

--- Front_End/js/user.js
+++ Front_End/js/user.js
@@ -122,6 +122,16 @@ $(document).ready(function (){
     // post add
     $("#postAdData").on("click", function (e) {
 
+        if (isNaN(price) || price <= 0) {
+            new Noty({
+                type: "error",
+                layout: "topRight",
+                text: "Price should be a valid positive number.",
+                timeout: 3000
+            }).show();
+            return;
+        }
+
         // Prepare DTO
         let adData = {
             title: $("#title").val(),
